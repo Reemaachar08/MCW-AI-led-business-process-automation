b3efc6329c58e4ed39109f613c13ac4f4bb9939c
Reemaachar08 <135792843+Reemaachar08@users.noreply.github.com>
2023-06-08 22:58:19 +0530

    Update post-provisioning.ps1

--- Hands-on lab/lab-files/ARM-template/post-provisioning.ps1
+++ Hands-on lab/lab-files/ARM-template/post-provisioning.ps1
@@ -1,38 +1,31 @@
-$subs = Get-AzSubscription | Select-Object -ExpandProperty Name
-if($subs.GetType().IsArray -and $subs.length -gt 1){
-    $subOptions = [System.Collections.ArrayList]::new()
-    for($subIdx=0; $subIdx -lt $subs.length; $subIdx++){
-            $opt = New-Object System.Management.Automation.Host.ChoiceDescription "$($subs[$subIdx])", "Selects the $($subs[$subIdx]) subscription."   
-            $subOptions.Add($opt)
-    }
-    $selectedSubIdx = $host.ui.PromptForChoice('Enter the desired Azure Subscription for this lab','Copy and paste the name of the subscription to make your choice.', $subOptions.ToArray(),0)
-    $selectedSubName = $subs[$selectedSubIdx]
-    Write-Information "Selecting the $selectedSubName subscription"
-    Select-AzSubscription -SubscriptionName $selectedSubName
-}
 $subscriptionId = (Get-AzContext).Subscription.Id
-$dest = (Get-Item .).parent.FullName
-
-$resourceGroupName = Read-Host -Prompt "Enter the name of the resource group you have created for your MCW environment."
-$storageAccountName = (Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName).StorageAccountName
+$dest = (Get-Item .).Parent.FullName
+$resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*RG01-*" }).ResourceGroupName
+$storageAccountName = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName).StorageAccountName
 $storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName).Value[0]
-$destinationContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
-$containerSASURI = New-AzStorageContainerSASToken -Context $destinationContext -ExpiryTime(get-date).AddSeconds(3600) -FullUri -Name "claimstraining" -Permission rw
-$localPath = $dest + "/claims-forms/*"
+$containerName = "claimstraining"
+$containerSAS = New-AzStorageContainerSASToken -Context (New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey) -ExpiryTime (Get-Date).AddSeconds(3600) -Name $containerName -Permission rw
+$containerSASURI = "https://$storageAccountName.blob.core.windows.net/$containerName$($containerSAS.TrimStart('?'))"
+$localPath = "C:\MCW\MCW-main\Hands-on lab\lab-files\claims-forms"
 
 Write-Host "Uploading training data to Azure Storage"
+$destinationContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+Get-ChildItem -Path $localPath -File -Exclude *test*.* | ForEach-Object {
+    Set-AzStorageBlobContent -Context $destinationContext -Container $containerName -File $_.FullName -Force
+}
 
-azcopy copy $localPath $containerSASURI --exclude-pattern="*test*.*"
-
-Write-Host "Deploying Document Processing Azure Function App "
+Write-Host "Deploying Document Processing Azure Function App"
 $functionAppName = (Get-AzFunctionApp -ResourceGroupName $resourceGroupName).Name
-$zipArchiveFullPath = $dest + "/source-azure-functions/DocumentProcessing.zip"
-az functionapp deployment source config-zip -g "$($resourceGroupName)" -n "$($functionAppName)" --src "$($zipArchiveFullPath)" --subscription "$($subscriptionId)"   
+$zipArchiveFullPath = "C:\MCW\MCW-main\Hands-on lab\lab-files\source-azure-functions\DocumentProcessing.zip"
+$functionApp = Get-AzWebApp -ResourceGroupName $resourceGroupName -Name $functionAppName
+Publish-AzWebApp -ResourceGroupName $resourceGroupName -Name $functionAppName -ArchivePath $zipArchiveFullPath -Force
+Set-AzResource -ResourceId (Get-AzFunctionApp -ResourceGroupName $resourceGroupName -Name $functionAppName).Id -PropertyObject @{
+    "ScmType" = "LocalGit"
+} -Force
 
 Write-Host "Deploying Hospital Portal"
 $webAppName = (Get-AzWebApp -ResourceGroupName $resourceGroupName | Where-Object Name -like 'contoso-portal*').Name
-$zipArchiveFullPath = $dest + "/source-hospital-portal/contoso-web.zip"
-az webapp deployment source config-zip -g "$($resourceGroupName)" -n "$($webAppName)" --src "$($zipArchiveFullPath)" --subscription "$($subscriptionId)"   
+$zipArchiveFullPath = "C:\MCW\MCW-main\Hands-on lab\lab-files\source-hospital-portal\contoso-web.zip"
+Publish-AzWebapp -ResourceGroupName $resourceGroupName -Name $webAppName -ArchivePath $zipArchiveFullPath
 
 Write-Host "Environment setup complete." -ForegroundColor Green
-
